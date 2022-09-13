fc32c8f0c39808605329b09ed96055a53796cc4f
Starck43 <saloon.as@gmail.com>
2022-09-14 01:37:54 +0300

    v1.3.0 renamed to Appeals and fixed error with reg link

--- components/forum/appeals.js
+++ components/forum/appeals.js
@@ -7,7 +7,7 @@ import {AlertDialog} from "../UI/dialogs"
 import DATA from "../../core/constants"
 
 
-const BodyContent = ({id, content, cost, date_forum, reg_is_active, reg_form}) => {
+const Appeals = ({id, content, cost, date_forum, reg_is_active, reg_form}) => {
 	const [showForm, setShowForm] = useState(false)
 	const [isRegOpen, setRegOpen] = useState(false)
 	const router = useRouter()
@@ -18,18 +18,20 @@ const BodyContent = ({id, content, cost, date_forum, reg_is_active, reg_form}) =
 		let available = reg_is_active && curDate <= forumDate
 		setRegOpen(available)
 		
-		if (router.asPath.startsWith(`/${router.query.id}?reg`)) {
+		if (router.asPath.endsWith(`?registration`)) {
 			setShowForm(true)
 		}
 
+		// for external links pressed
+		let openRegistration = () => {
+			setShowForm(true)
+		}
 		
-/*		let openRegistration = () => setShowForm(true)
-		
-		const link = document.getElementById("registration")
+		const link = document.getElementById("regLink")
 		if (link) {
 			link.addEventListener("click", openRegistration)
 			return () => link.removeEventListener("click", openRegistration)
-		}*/
+		}
 
 	}, [])
 
@@ -40,18 +42,18 @@ const BodyContent = ({id, content, cost, date_forum, reg_is_active, reg_form}) =
 
 	return (
 		<section className="appeals">
-			<div className="appeals-container flex-column p-4 mx-auto">
-				
+			<div className="appeals-container flex-column p-4 my-4vh mx-auto">
+
 				<HtmlContent className="appeal-body">
 					{content}
 				</HtmlContent>
 				
 				{isRegOpen &&
-				<div className="appeal-footer large-text mt-4 mx-auto">
+				<div className="appeal-footer mt-4 mx-auto">
 					<div className="appeal-reg-info">
 						<HtmlContent>{DATA.reg_content}</HtmlContent>
 						<b>
-							** Участие в мероприятии <span className="highlight">{`${cost ? "платное" : "бесплатное"}`}</span>
+							** Участие в мероприятии <span className="highlight">{`${cost ? "платное" : "бесплатное"}!`}</span>
 						</b>
 					</div>
 					<div className="button" onClick={handleRegistrationClick}>Зарегистрироваться online</div>
@@ -81,4 +83,4 @@ const BodyContent = ({id, content, cost, date_forum, reg_is_active, reg_form}) =
 	)
 }
 
-export default BodyContent
+export default Appeals
