8adf2e7de974963ad9dd4ae300468e8e3d39713c
Starck43 <saloon.as@gmail.com>
2022-09-14 01:33:22 +0300

    v1.3.0 modified code for Avatar and removed some func to utils

--- components/UI/avatar.js
+++ components/UI/avatar.js
@@ -1,6 +1,6 @@
 import {useState} from "react"
 import Image from "next/image"
-//import placeholder from "~/public/placeholder.png"
+import {shimmer, toBase64} from "../../core/utils"
 
 
 export const Logo = ({className = "logo", name = "", src}) => {
@@ -16,28 +16,6 @@ export const Logo = ({className = "logo", name = "", src}) => {
 	const loadComplete = function (imageDimension) {
 		setImageSize(imageDimension)
 	}
-
-	const shimmer = (color, w, h = null) => `
-		<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		<defs>
-		<linearGradient id="g">
-		<stop stop-color="${color}" offset="20%" />
-		<stop stop-color="#ccc" offset="50%" />
-		<stop stop-color="${color}" offset="70%" />
-		</linearGradient>
-		</defs>
-		${h == null
-		? `<circle fill="${color}" cx="${w / 2}" cy="${w / 2}" r="${w / 2}"/><circle id="c" fill="url(#g)" cx="${w / 2}" cy="${w / 2}" r="${w / 2}"/>`
-		: `<rect width="${w}" height="${h}" fill="${color}" /><rect id="r" width="${w}" height="${h}" fill="url(#g)" />`
-	}
-		<animate xlink:href="#c" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"/>
-		</svg>`
-
-	const toBase64 = (str) => (
-		typeof window === "undefined"
-			? Buffer.from(str).toString("base64")
-			: window.btoa(str)
-	)
 
 	return (
 		<div className={className}>
@@ -58,20 +36,20 @@ export const Logo = ({className = "logo", name = "", src}) => {
 	)
 }
 
-export const Avatar = ({src, name = "", width = 160, height = null, rounded = "rounded"}) => {
+export const Avatar = ({src, name = "", width = 160, height = null, rounded = "rounded", className}) => {
 
 	return (
-		<div className="avatar centered vertical">
+		<div className={`avatar centered vertical ${rounded} ${className}`}>
 			<Image
-				className={rounded}
 				src={src}
+				alt={name}
 				width={width}
 				height={height ? height : width}
-				blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer("#a6a6a6", width, height))}`}
 				placeholder="blur"
-				alt={name}
+				blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer("#a6a6a6", width, height))}`}
+				unoptimized
 			/>
-			<div className="avatar-caption">{name}</div>
+			{name && <div className="avatar-caption">{name}</div>}
 		</div>
 	)
 }
