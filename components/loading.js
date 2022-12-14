eff9c50ab496ea3469d91354de641c6811f44f08
Starck43 <saloon.as@gmail.com>
2022-12-15 01:27:26 +0200

    v1.3.4 updated dependencies, removed unused, updated max-width for logo in footer

--- components/loading.js
+++ components/loading.js
@@ -1,18 +1,14 @@
-import styled from 'styled-components/macro'
+import {memo} from "react"
 
+const Loading = () => <div style={{
+  position: "fixed",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+  transition: "all 150ms ease",
+  backgroundColor: "rgba(255,255,255, .3)",
+  zIndex: 9999
+}}/>
 
-const Loading = () => <Container/>
-
-export default Loading
-
-
-const Container = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  transition: all 150ms ease;
-  background-color: rgba(255,255,255, .5);
-  z-index: 9999;
-`
+export default memo(Loading)
