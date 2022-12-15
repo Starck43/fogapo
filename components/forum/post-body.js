773b04162f2e97c2280719160da07b8baf2c1786
Starck43 <saloon.as@gmail.com>
2022-12-15 13:12:43 +0200

    v1.3.4 revalidation time decreased and added legacyBehavior for Link, replaced background on the page body

--- components/forum/post-body.js
+++ components/forum/post-body.js
@@ -34,44 +34,56 @@ function PostBody({post}) {
 	}
 
 	return (
-		<main className="post post-body">
+		<main
+			className="post post-body"
+			style={{
+				backgroundImage: `url(${background})`,
+				backgroundPosition: "right bottom",
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "contain"
+			}}
+		>
 
 			{date_forum &&
-			<section
-				className={`appeals-forum bg-color-secondary ${events?.length === 0 ? "pt-4vh" : "py-4vh"}`}
-				style={{
-					//backgroundImage: `url(${background})`,
-					//backgroundPosition: "right bottom",
-					//backgroundRepeat: "no-repeat",
-					//backgroundSize: "contain"
-				}}
-			>
+			<section className={`appeals-forum bg-color-secondary ${events?.length === 0 ? "pt-4vh" : "py-4vh"}`}>
 				<Appeals isActive={isActive} {...post}/>
 			</section>
 			}
 
 			{events?.length > 0 &&
 			<section className="events-forum bg-color-brand shadow4 py-4vh">
-				<Events events={events} className="events-container px-4vw my-4"/>
+				<Events
+					events={events}
+					className="events-container px-4vw my-4"
+				/>
 			</section>
 			}
 
 			{(partners?.length > 0 || location || info) &&
-			<section
-				className="info-forum bg-color-secondary bg-image py-4vh"
-				style={{backgroundImage: `url(${background})`}}
-			>
+			<section className="info-forum bg-color-secondary py-4vh">
 				<Container className="info-container px-4vw flex-wrap">
-					<Partners partners={partners} showTitle defaultType="name" className="partners-block my-4"/>
-					<Contacts location={location} info={info} isActive={isActive}
-					          className="contacts-block flex-column my-4"/>
+					<Partners
+						partners={partners}
+						showTitle
+						defaultType="name"
+						className="partners-block my-4"
+					/>
+					<Contacts
+						location={location}
+						info={info}
+						isActive={isActive}
+						className="contacts-block flex-column my-4"
+					/>
 				</Container>
 			</section>
 			}
 
 			{reviews?.length > 0 &&
 			<section className="reviews-forum bg-color-primary py-4vh">
-				<Reviews reviews={reviews} className="reviews-container px-4vw my-4"/>
+				<Reviews
+					reviews={reviews}
+					className="reviews-container px-4vw my-4"
+				/>
 			</section>
 			}
 
