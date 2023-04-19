6799eb10e9c637893337905d5c58699032bb9afb
Starck43 <saloon.as@gmail.com>
2023-04-19 23:58:25 +0300

    check undefined calendar data before loading

--- components/calendar/calendar-dialog.js
+++ components/calendar/calendar-dialog.js
@@ -2,70 +2,72 @@ import { AlertDialog } from "../UI/dialogs"
 import { GroupItems } from "./group-items"
 import { Accordion } from "react-bootstrap"
 import { useFetch } from "../../core/Fetch"
-import SubTitle from "../UI/subtitle"
 import { TbLoader3 as Loader } from "react-icons/tb"
+import Title from "../UI/title"
 
 export function CalendarDialog({ expandedId, show, closeHandler }) {
     const { data, error } = useFetch(process.env.API_SERVER, process.env.API_ENDPOINTS.postsGrouped)
 
     if (error) {
-        return <SubTitle>Ошибка загрузки мероприятий. Попробуйте позже!</SubTitle>
+        return <Title title="Ошибка загрузки мероприятий." subTitle="Попробуйте позже" />
     }
-
-    const { prev_forums, next_forums } = data
 
     return (
         <AlertDialog
             title="Календарь мероприятий"
             show={show}
             closeHandler={closeHandler}
-            className="events-calendar-modal"
+            className="forum-calendar-modal"
             size="md"
             scrollable
         >
             {data ? (
                 <>
-                    <div className={`forum-group next`}>
-                        {/*<h4 className="title">Предстоящие мероприятия</h4>*/}
-                        <ul className="next-forums">
-                            {next_forums?.map((item, index) => (
-                                <GroupItems
-                                    key={item.id}
-                                    currentIndex={index}
-                                    selected={expandedId}
-                                    closeHandler={closeHandler}
-                                    {...item}
-                                />
-                            ))}
-                        </ul>
-                    </div>
+                    {data.next_forums?.length ? (
+                        <div className={`forum-group next`}>
+                            {/*<h4 className="title">Предстоящие мероприятия</h4>*/}
+                            <ul className="next-forums">
+                                {data.next_forums.map((item, index) => (
+                                    <GroupItems
+                                        key={item.id}
+                                        currentIndex={index}
+                                        selected={expandedId}
+                                        closeHandler={closeHandler}
+                                        {...item}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                    ) : null}
 
-                    <div className={`forum-group prev`}>
-                        <Accordion
-                            className="prev-forums-accordion"
-                            defaultActiveKey={expandedId.toString()}
-                            alwaysOpen
-                        >
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>
-                                    <div className="title">Архив мероприятий</div>
-                                </Accordion.Header>
-                                <Accordion.Body>
-                                    <ul className="prev-forums">
-                                        {prev_forums?.map((item, index) => (
-                                            <GroupItems
-                                                key={item.id}
-                                                currentIndex={index}
-                                                selected={expandedId}
-                                                closeHandler={closeHandler}
-                                                {...item}
-                                            />
-                                        ))}
-                                    </ul>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </div>
+                    {data.prev_forums?.length ? (
+                        <div className={`forum-group prev`}>
+                            <Accordion
+                                className="prev-forums-accordion"
+                                defaultActiveKey={expandedId.toString()}
+                                alwaysOpen
+                            >
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>
+                                        <div className="title">Архив мероприятий</div>
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                        <ul className="prev-forums">
+                                            {data.prev_forums.map((item, index) => (
+                                                <GroupItems
+                                                    key={item.id}
+                                                    currentIndex={index}
+                                                    selected={expandedId}
+                                                    closeHandler={closeHandler}
+                                                    {...item}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                        </div>
+                    ) : null}
                 </>
             ) : (
                 <Loader className="loader-icon" />
--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -1,13 +1,6 @@
-import { Fragment, memo, useCallback, useState } from "react"
-
+import { memo, useCallback, useState } from "react"
 import { AiFillCalendar as CalendarIcon } from "react-icons/ai"
-import { TbLoader3 as Loader } from "react-icons/tb"
-import { Accordion } from "react-bootstrap"
 
-import Items from "./group-items"
-import { AlertDialog } from "../UI/dialogs"
-import { Fetch, useFetch } from "../../core/Fetch"
-import SubTitle from "../UI/subtitle"
 import { CalendarDialog } from "./calendar-dialog"
 
 const Calendar = ({ selectedId = 0 }) => {
@@ -17,7 +10,7 @@ const Calendar = ({ selectedId = 0 }) => {
     }, [show])
 
     return (
-        <Fragment>
+        <>
             <div className="calendar-button centered" onClick={handleCloseDialog}>
                 <CalendarIcon />
                 <span>Календарь мероприятий</span>
@@ -30,7 +23,7 @@ const Calendar = ({ selectedId = 0 }) => {
                     closeHandler={handleCloseDialog}
                 />
             )}
-        </Fragment>
+        </>
     )
 }
 
