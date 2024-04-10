3208c107aedd3187fd968d847330a3f9c9ec1fa6
Starck43 <saloon.as@gmail.com>
2024-04-10 22:56:21 +0300

    formatted file

--- backend/crm/urls.py
+++ backend/crm/urls.py
@@ -8,18 +8,18 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-	path('', RedirectView.as_view(url='/admin')),
+    path('', RedirectView.as_view(url='/admin')),
     path('admin/', admin.site.urls),
-	path('api/', include('forum.urls')),
-	re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
-	path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
-	path('generate_new_pages/', views.generate_pages),
+    path('api/', include('forum.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    path('generate_new_pages/', views.generate_pages),
 ]
 
-
 if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-	import debug_toolbar
-	urlpatterns = [
-		path('__debug__/', include(debug_toolbar.urls)),
-	] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
